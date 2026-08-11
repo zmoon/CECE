e6c8842954e485f1f41802b0dcb5f0bae88e3df8
zmoon <zachary.moon@noaa.gov>
2026-08-11 14:40:46 +0000

    Configure AMIO staging buffer from CECE

--- src/driver/cece_driver_facade.cpp
+++ src/driver/cece_driver_facade.cpp
@@ -740,10 +740,61 @@ bool CeceDriverOrchestrator::AdvanceTime(const std::string& time_iso8601, void* 
         std::string active_data_model = data_models_to_try.front();
 
         int amio_threads = 1;
+        int amio_read_timeout_s = 120;
+        int amio_staging_timeout_ms = 30000;
+        int amio_staging_buffer_count = 8;
+        int amio_prefetch_depth = 2;
+        std::uint64_t amio_staging_buffer_capacity_bytes = 268435456ULL;  // 256 MiB per staging buffer
         if (config["driver"] && config["driver"]["amio_worker_threads"]) {
             amio_threads = config["driver"]["amio_worker_threads"].as<int>();
             if (amio_threads < 1) {
                 amio_threads = 1;
+            }
+        }
+        if (config["driver"] && config["driver"]["amio_read_timeout_s"]) {
+            amio_read_timeout_s = config["driver"]["amio_read_timeout_s"].as<int>();
+            if (amio_read_timeout_s < 1) {
+                amio_read_timeout_s = 1;
+            }
+            if (amio_read_timeout_s > 3600) {
+                amio_read_timeout_s = 3600;
+            }
+        }
+        if (config["driver"] && config["driver"]["amio_staging_timeout_ms"]) {
+            amio_staging_timeout_ms = config["driver"]["amio_staging_timeout_ms"].as<int>();
+            if (amio_staging_timeout_ms < 1) {
+                amio_staging_timeout_ms = 1;
+            }
+            if (amio_staging_timeout_ms > 60000) {
+                amio_staging_timeout_ms = 60000;
+            }
+        }
+        if (config["driver"] && config["driver"]["amio_staging_buffer_count"]) {
+            amio_staging_buffer_count = config["driver"]["amio_staging_buffer_count"].as<int>();
+            if (amio_staging_buffer_count < 1) {
+                amio_staging_buffer_count = 1;
+            }
+            if (amio_staging_buffer_count > 4096) {
+                amio_staging_buffer_count = 4096;
+            }
+        }
+        if (config["driver"] && config["driver"]["amio_prefetch_depth"]) {
+            amio_prefetch_depth = config["driver"]["amio_prefetch_depth"].as<int>();
+            if (amio_prefetch_depth < 1) {
+                amio_prefetch_depth = 1;
+            }
+            if (amio_prefetch_depth > 1024) {
+                amio_prefetch_depth = 1024;
+            }
+        }
+        if (config["driver"] && config["driver"]["amio_staging_buffer_capacity_bytes"]) {
+            const std::uint64_t requested_capacity = config["driver"]["amio_staging_buffer_capacity_bytes"].as<std::uint64_t>();
+            amio_staging_buffer_capacity_bytes = requested_capacity;
+            if (amio_staging_buffer_capacity_bytes < 1ULL) {
+                amio_staging_buffer_capacity_bytes = 1ULL;
+            }
+            if (amio_staging_buffer_capacity_bytes > 1073741824ULL) {
+                amio_staging_buffer_capacity_bytes = 1073741824ULL;  // 1024 MiB (1 GiB) per staging buffer
             }
         }
 
@@ -757,14 +808,14 @@ bool CeceDriverOrchestrator::AdvanceTime(const std::string& time_iso8601, void* 
                        << "path: " << input_file_path << "\n"
                        << "data_model: " << candidate_model << "\n"
                        << "staging_pool:\n"
-                       << "  buffer_count: 8\n"
-                       << "  buffer_capacity_bytes: 268435456\n"
+                       << "  buffer_count: " << amio_staging_buffer_count << "\n"
+                       << "  buffer_capacity_bytes: " << amio_staging_buffer_capacity_bytes << "\n"
                        << "worker_pool:\n"
                        << "  threads: " << amio_threads << "\n"
                        << "prefetch:\n"
-                       << "  depth: 2\n"
-                       << "  read_timeout_s: 120\n"
-                       << "staging_timeout_ms: 30000\n";
+                       << "  depth: " << amio_prefetch_depth << "\n"
+                       << "  read_timeout_s: " << amio_read_timeout_s << "\n"
+                       << "staging_timeout_ms: " << amio_staging_timeout_ms << "\n";
                 m_file.close();
             }
 
